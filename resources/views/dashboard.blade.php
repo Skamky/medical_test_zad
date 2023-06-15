256997cee81cec93798cba2b3dc11b34c8f33b07
Skamky_H <moskam@tuta.io>
2023-06-15 12:56:47 +0500

    переустановака бриза

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,32 +1,32 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Главная') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Добро Пожаловать !") }}
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("Добро пожаловать!") }}
                 </div>
             </div>
         </div>
     </div>
 
     <h2>Список Врачей</h2>
-<table class="table">
-    <thead>
-    <tr>
-        <th scope="col">#</th>
-        <th scope="col">Имя</th>
-        <th scope="col">Фамилия</th>
-        <th scope="col">Отчество</th>
-    </tr>
-    </thead>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Имя</th>
+            <th scope="col">Фамилия</th>
+            <th scope="col">Отчество</th>
+        </tr>
+        </thead>
 
-</table>
+    </table>
     <form>
 
     </form>
